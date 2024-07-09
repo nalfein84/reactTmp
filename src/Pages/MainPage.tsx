810986b8a988f54720b068cc0e22bfc67aec2374
ybattis <yacine.battis@groupe-berto.com>
2024-07-09 17:31:31 +0200

    Modification des donnees sources et ajustement affichage

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -2,26 +2,23 @@ import React from 'react';
 import ProductTable from '../organisms/ProductTable';
 import { View } from 'react-native';
 import { IProduct } from '../atoms/ProductData';
+import { SearchBar } from '../molecules/SearchBar';
 
 export function MainPage() : React.ReactNode {
     return (
         <View>
+            <SearchBar />
             <ProductTable products={PRODUCTS} />
         </View>
     );
 }
 
+
 const PRODUCTS: IProduct[] =  [
-  {
-    "id":1,
-    "imgUrl": "../../images/lettuce.jpg",
-    "name": "Lettuce",
-    "desc": "description lettuce"
-  },
-  {
-    "id":2,
-    "imgUrl": "images/carrot.jpg",
-    "name": "Carrot",
-    "desc": "description carrot"
-  }
-]
+    {desc: "Fruits", imgUrl: "$1", id: 1, name: "Apple"},
+    {desc: "Fruits", imgUrl: "$1", id: 2, name: "Dragonfruit"},
+    {desc: "Fruits", imgUrl: "$2", id: 3, name: "Passionfruit"},
+    {desc: "Vegetables", imgUrl: "$2", id: 4, name: "Spinach"},
+    {desc: "Vegetables", imgUrl: "$4", id: 5, name: "Pumpkin"},
+    {desc: "Vegetables", imgUrl: "$1", id: 6, name: "Peas"}
+  ]
